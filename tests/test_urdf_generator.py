a36156193977fe585203452712cf5f202a0a72c0
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2024-12-30 17:19:07 -0800

    feat: add robot joint description

--- tests/test_urdf_generator.py
+++ tests/test_urdf_generator.py
@@ -16,3 +16,32 @@ class TestURDFGenerator(unittest.TestCase):
             '</robot>'
         )
         self.assertEqual(urdfg.generate_urdf(robot_description), expected_output)
+
+    def test_generate_urdf_with_joints(self):
+        urdfg = URDFGenerator()
+        robot_description = {
+            "name": "test_robot",
+            "links": [{"name": "base_link"}, {"name": "link1"}],
+            "joints": [{
+                "name": "joint1",
+                "type": "revolute",
+                "parent": "base_link",
+                "child": "link1",
+                "origin": {"rpy": "0 0 0", "xyz": "0 0 0"},
+                "axis": "0 0 1",
+            }]
+        }
+        expected_output = (
+            '<?xml version="1.0" ?>\n'
+            '<robot name="test_robot">\n'
+            '  <link name="base_link" />\n'
+            '  <link name="link1" />\n'
+            '  <joint name="joint1" type="revolute">\n'
+            '    <parent link="base_link" />\n'
+            '    <child link="link1" />\n'
+            '    <origin rpy="0 0 0" xyz="0 0 0" />\n'
+            '    <axis xyz="0 0 1" />\n'
+            '  </joint>\n'
+            '</robot>'
+        )
+        self.assertEqual(urdfg.generate_urdf(robot_description), expected_output)
--- urdf_writer/urdf_generator.py
+++ urdf_writer/urdf_generator.py
@@ -2,8 +2,24 @@ class URDFGenerator:
     def generate_urdf(self, robot_description):
         name = robot_description["name"]
         links = robot_description["links"]
+        joints = robot_description["joints"]
+
         urdf = f'<?xml version="1.0" ?>\n<robot name="{name}">\n'
+
+        # Add links
         for link in links:
             urdf += f'  <link name="{link["name"]}" />\n'
+
+        # Add joints
+        for joint in joints:
+            urdf += (
+                f'  <joint name="{joint["name"]}" type="{joint["type"]}">\n'
+                f'    <parent link="{joint["parent"]}" />\n'
+                f'    <child link="{joint["child"]}" />\n'
+                f'    <origin rpy="{joint["origin"]["rpy"]}" xyz="{joint["origin"]["xyz"]}" />\n'
+                f'    <axis xyz="{joint["axis"]}" />\n'
+                f'  </joint>\n'
+            )
+
         urdf += '</robot>'
         return urdf
